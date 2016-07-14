80f0f7bf1a740aef2e64f32e79cbfcba909869b6
fish <fish@deb-AMG-ghost.deb-AMG-ghost.net>
2016-07-14 21:48:47 +0200

    d06ex01

---
+++ d06/ex01/ft_print_program_name.c
@@ -0,0 +1,8 @@
+#include	"libft.h"
+
+int		main(int argc, char **argv)
+{
+    if (argc)
+	ft_putstr(argv[0]);
+    return (0);
+}
---
+++ d06/ex01/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzafati <wzafati@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/14 19:10:07 by wzafati           #+#    #+#             */
+/*   Updated: 2016/07/14 19:10:55 by wzafati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		LIBFT_H
+# define	LIBFT_H
+
+# include	<unistd.h>
+
+void		ft_putchar(char c);
+void		ft_swap(int *a, int *b);
+void		ft_putstr(char *str);
+void		ft_putnbr(int nbr);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_atoi(char *str);
+
+#endif
