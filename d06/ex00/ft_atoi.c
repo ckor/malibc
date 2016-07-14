386197b746e5f51dda491102838b728bf5c1fc6e
fish <fish@deb-AMG-ghost.deb-AMG-ghost.net>
2016-07-14 19:47:44 +0200

    d06

---
+++ d06/ex00/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzafati <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/10 03:14:17 by wzafati           #+#    #+#             */
+/*   Updated: 2016/07/10 08:00:17 by wzafati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+unsigned int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9') ? (1) : (0);
+}
+
+int		ft_isspace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' ||
+		c == '\r' || c == '\f' || c == '\v') ? (1) : (0);
+}
+
+int		ft_atoi(char *str)
+{
+	int s;
+	int n;
+
+	n = 0;
+	while (ft_isspace(*str))
+		str = str + 1;
+	s = (*str == '-') ? (-1) : (1);
+	str = str + (*str ==  '-' || *str == '+');
+	while ((ft_isdigit(*str)) && (n = 10 * n + (*str - '0')))
+		str = str + 1;
+	return (s * n);
+}
---
+++ d06/ex00/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzafati <wzafati@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/14 19:10:07 by tgauvrit          #+#    #+#             */
+/*   Updated: 2016/07/14 19:10:55 by tgauvrit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		LIBFT_H
+# define	LIBFT_H
+
+#include	<unistd.h>
+#include	<stdlib.h>
+
+void		ft_putchar(char c);
+void		ft_swap(int *a, int *b);
+void		ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
