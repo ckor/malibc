35a515d5c6f56525d52747fe032fb09dd690deca
fish <fish@deb-AMG-ghost.deb-AMG-ghost.net>
2016-07-15 04:24:58 +0200

    d08ex01

--- d08/ex01/ft_boolean.h
+++ d08/ex01/ft_boolean.h
@@ -12,7 +12,12 @@
 
 #ifndef		FT_BOOLEAN_H
 # define	FT_BOOLEAN_H
-
-
-
+# include	<unistd.h>
+# define	EVEN(x) x % 2 == 0
+# define	TRUE		1
+# define	FALSE		0
+# define	SUCCESS		0
+# define	EVEN_MSG	"I have an even number of arguments."
+# define	ODD_MSG		"I have an odd number of arguments."
+typedef		int		t_bool;
 #endif
