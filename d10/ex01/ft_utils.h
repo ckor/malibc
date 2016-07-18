6e66e4a02646e69ad95b5eb835d79e2bbed60c29
CKOR <wzafati@student.42.fr>
2016-07-18 17:16:50 +0200

    d10ex01

---
+++ d10/ex01/ft_utils.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzafati <wzafati@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/18 12:20:19 by wzafati           #+#    #+#             */
+/*   Updated: 2016/07/18 12:20:21 by wzafati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_UTILS_H
+# define FT_UTILS_H
+# include <unistd.h>
+# include <stdlib.h>
+
+void			ft_putchar(char c);
+int				ft_strlen(char *str);
+void			ft_putstr(char *str);
+unsigned int	ft_isdigit(char c);
+int				ft_isneg(int nbr);
+int				ft_isspace(char c);
+int				ft_atoi(char *str);
+void			ft_putnbr(int nbr);
+int				ft_recursive_power(int nb, int power);
+void			ft_foreach(int *tab, int length, void(*f)(int));
+int				*ft_map(int *tab, int length, int(*f)(int));
+#endif
---
+++ d10/ex01/main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzafati <wzafati@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/18 10:38:16 by wzafati           #+#    #+#             */
+/*   Updated: 2016/07/18 10:38:18 by wzafati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_utils.h"
+
+int			*ft_args_to_inttab(char **av, int ac)
+{
+	int		*tab;
+	int		i;
+
+	i = 1;
+	if ((tab = malloc(sizeof(*tab) * (ac - 1))) == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		tab[i - 1] = ft_atoi(av[i]);
+		i++;
+	}
+	return (tab);
+}
+
+void		ft_print_number(int nbr)
+{
+	ft_putnbr(nbr);
+	ft_putchar('\n');
+}
+
+int			main(int ac, char **av)
+{
+	int		*tab;
+
+	if (ac > 1)
+	{
+		tab = ft_args_to_inttab(av, ac);
+		ft_foreach(tab, (ac - 1), &ft_print_number);
+	}
+	return (0);
+}
