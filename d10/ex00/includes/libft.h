3f4ba2d59658a544f2bdea003aeb0be1c9de70eb
CKOR <wzafati@student.42.fr>
2016-07-18 09:47:48 +0200

    d10ex00

---
+++ d10/ex00/includes/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzafati <wzafati@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/14 19:10:07 by wzafati           #+#    #+#             */
+/*   Updated: 2016/07/14 19:10:55 by wzafati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+int			ft_strcmp(char *s1, char *s2);
+int			ft_strlen(char *str);
+void		ft_swap(int *a, int *b);
+#endif
